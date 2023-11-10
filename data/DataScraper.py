42811e47cb85762d0fbf7581dd983f1242fc1800
DaronAdams <daron.cole@icloud.com>
2023-11-10 16:49:28 -0600

    Feat: Finish API to CSV conversion.

--- data/DataScraper.py
+++ data/DataScraper.py
@@ -1,6 +1,8 @@
+import json
 import psycopg2
 import requests
 import os
+import csv
 
 from dotenv import load_dotenv
 
@@ -18,48 +20,62 @@ class ApiHandler():
         }
         response = requests.get(self.api_endpoint, headers=headers)
         if response.status_code == 200:
-            return response.json()
+            return json.loads(response.content)
         else:
             response.raise_for_status()
 
-    def save_to_database(self, data):
-        records_to_insert = []
+    def write_to_csv(self, first_row=None):
+        json_data = self.call_api()
+        with open("market_data.csv", "a", newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(
+                ["item_id", "avg_high_price", "high_price_volume", "avg_low_price", "low_price_volume",
+                 "timestamp"])
 
-        # Extract the timestamp which is common for all entries
-        timestamp = data['timestamp']
+            for symbol, data in json_data["data"].items():
+                avg_high_price = data["avgHighPrice"]
+                high_price_volume = data["highPriceVolume"]
+                avg_low_price = data["avgLowPrice"]
+                low_price_volume = data["lowPriceVolume"]
+                timestamp = json_data["timestamp"]
 
-        # Iterate over the items excluding the timestamp
-        for id, stats in data.items():
-            if id != 'timestamp':  # Skip the timestamp entry
-                record = (
-                    id,
-                    stats.get('avgHighPrice'),
-                    stats.get('highPriceVolume'),
-                    stats.get('avgLowPrice'),
-                    stats.get('lowPriceVolume'),
-                    timestamp
-                )
-                records_to_insert.append(record)
+                writer.writerow(
+                    [symbol, avg_high_price, high_price_volume, avg_low_price, low_price_volume, timestamp])
 
-        # SQL to insert data
-        insert_query = '''
-        INSERT INTO market_data (id, avg_high_price, high_price_volume, avg_low_price, low_price_volume, timestamp)
-        VALUES (?, ?, ?, ?, ?, ?)
-        '''
+            # Close the CSV file
+            csvfile.close()
 
-        try:
-            conn = psycopg2.connect(self.db_path)
-            cursor = conn.cursor()
-            cursor.executemany(insert_query, records_to_insert)
-            conn.commit()
-            conn.close()
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    # def save_to_database(self, data):
+    #     with psycopg2.connect(self.db_path) as conn:
+    #         cursor = conn.cursor()
+    #
+    #         insert_query = '''
+    #             INSERT INTO market_data (item_id, avg_high_price, high_price_volume, avg_low_price, low_price_volume, timestamp)
+    #             VALUES (%s, %s, %s, %s, %s, %s)
+    #         '''
+    #
+    #         timestamp = data["timestamp"]
+    #
+    #         for symbol, data in data["data"].items():
+    #             avg_high_price = data["avgHighPrice"]
+    #             high_price_volume = data["highPriceVolume"]
+    #             avg_low_price = data["avgLowPrice"]
+    #             low_price_volume = data["lowPriceVolume"]
+    #
+    #         cursor.execute(insert_query,
+    #                        (symbol, avg_high_price, high_price_volume, avg_low_price, low_price_volume, timestamp))
 
-    def update_database_with_api_data(self):
-        data = self.call_api()
+    # def update_database_with_api_data(self):
+    #     data = self.call_api()
+    #
+    #     self.save_to_database(data)
 
-        # Transform the data as necessary to fit your database schema.
-        # This is a placeholder for the data transformation logic.
-        # transformed_data = [(item['field1'], item['field2']) for item in data]
-        self.save_to_database(data)
+    # def print_data(self):
+    #     with psycopg2.connect(self.db_path) as conn:
+    #         cursor = conn.cursor()
+    #
+    #         cursor.execute("SELECT * FROM market_data")
+    #
+    #         records = cursor.fetchall()
+    #         for record in records:
+    #             print(record)
--- data/main.py
+++ data/main.py
@@ -1,8 +1,7 @@
 from DataScraper import ApiHandler
 
 # Constants ----------------------------------------------------------------
-api_endpoint = "https://prices.runescape.wiki/api/v1/osrs/5m"
+api_endpoint = "https://prices.runescape.wiki/api/v1/osrs/1h"
 
 handler = ApiHandler(api_endpoint)
-handler.update_database_with_api_data()
-
+handler.write_to_csv()
