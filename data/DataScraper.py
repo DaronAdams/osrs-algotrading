3ba04244aebad9455af67243160b8e05e5d1f9d5
DaronAdams <daron.cole@icloud.com>
2023-11-10 12:22:17 -0600

    Updated gitignore, made new venv

--- data/DataScraper.py
+++ data/DataScraper.py
@@ -1,12 +1,16 @@
+import psycopg2
 import requests
-import sqlite3
 import os
 
+from dotenv import load_dotenv
+
+
 class ApiHandler():
-    def __init__(self, api_endpoint, db_path) -> None:
+    def __init__(self, api_endpoint) -> None:
+        load_dotenv()
         self.api_endpoint = api_endpoint
-        self.db_path = db_path
-    
+        self.db_path = os.getenv("DB_URI")
+
     def call_api(self):
         headers = {
             'User-Agent': 'Osrs AlgoTrading',
@@ -19,14 +23,8 @@ class ApiHandler():
             response.raise_for_status()
 
     def save_to_database(self, data):
-
-        db_dir = os.path.dirname(self.db_path)
-        if not os.path.exists(db_dir):
-            os.makedirs(db_dir)
-
-
         records_to_insert = []
-        
+
         # Extract the timestamp which is common for all entries
         timestamp = data['timestamp']
 
@@ -42,26 +40,25 @@ class ApiHandler():
                     timestamp
                 )
                 records_to_insert.append(record)
-        
+
         # SQL to insert data
         insert_query = '''
         INSERT INTO market_data (id, avg_high_price, high_price_volume, avg_low_price, low_price_volume, timestamp)
         VALUES (?, ?, ?, ?, ?, ?)
         '''
-        
+
         try:
-            conn = sqlite3.connect(db_dir)
+            conn = psycopg2.connect(self.db_path)
             cursor = conn.cursor()
             cursor.executemany(insert_query, records_to_insert)
             conn.commit()
-        except sqlite3.Error as e:
-            print(f"An error occurred: {e.args[0]}")
-        #finally:
-         #   conn.close()
+            conn.close()
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def update_database_with_api_data(self):
         data = self.call_api()
-        
+
         # Transform the data as necessary to fit your database schema.
         # This is a placeholder for the data transformation logic.
         # transformed_data = [(item['field1'], item['field2']) for item in data]
--- data/main.py
+++ data/main.py
@@ -2,8 +2,7 @@ from DataScraper import ApiHandler
 
 # Constants ----------------------------------------------------------------
 api_endpoint = "https://prices.runescape.wiki/api/v1/osrs/5m"
-db_path = "/market_data.db"
 
-handler = ApiHandler(api_endpoint, db_path)
+handler = ApiHandler(api_endpoint)
 handler.update_database_with_api_data()
 
